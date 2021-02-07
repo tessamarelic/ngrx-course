4aff9b1d93a95944cb579e94ae7455571de50cc5
Tessa Marelic <tessamarelic@gmail.com>
2021-02-07 17:47:57 +1100

    First 8 tutorials

---
+++ src/app/auth/auth.actions.ts
@@ -0,0 +1,9 @@
+import {createAction, props} from '@ngrx/store';
+import {User} from './model/user.model';
+
+export const login = createAction(
+  '[Login Page] User Login',
+  props<{user: User}>()
+);
+
+// const newLoginAction = login({user: })
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
-import {Store} from "@ngrx/store";
-
-import {AuthService} from "../auth.service";
-import {tap} from "rxjs/operators";
-import {noop} from "rxjs";
-import {Router} from "@angular/router";
+import {AuthService} from '../auth.service';
+import {Router} from '@angular/router';
+import {tap} from 'rxjs/operators';
+import {noop} from 'rxjs';
+import {AppState} from '../../reducers';
+import {Store} from '@ngrx/store';
+import {login} from '../auth.actions';
 
 @Component({
   selector: 'login',
@@ -20,7 +21,8 @@ export class LoginComponent implements OnInit {
   constructor(
       private fb:FormBuilder,
       private auth: AuthService,
-      private router:Router) {
+      private router:Router,
+      private store: Store<AppState> ) {
 
       this.form = fb.group({
           email: ['test@angular-university.io', [Validators.required]],
@@ -34,7 +36,23 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-
+  const val = this.form.value;
+  this.auth.login(val.email, val.password)
+    .pipe(
+      // allows us to add a side effect such as storing to the store
+      tap(user => {
+        console.log(user);
+        this.store.dispatch(
+        login({user}));
+        this.router.navigateByUrl('/courses');
+      })
+    )
+    .subscribe(
+      noop,
+      () => {
+        alert('Login Failed');
+      }
+    );
   }
 
 }
